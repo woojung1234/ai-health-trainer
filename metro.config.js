6e545a3d0337652b5a92407b8b4d8b1649e26a88
woojung1234 <84664664+woojung1234@users.noreply.github.com>
2025-05-19 15:01:37 +0900

    metro.config.js 추가

---
+++ metro.config.js
@@ -0,0 +1,21 @@
+const {getDefaultConfig} = require('@react-native/metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
